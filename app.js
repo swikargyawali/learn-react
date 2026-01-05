c1851d44c752f10d98225a2c16c84a80a20e7139
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-05 20:55:26 +0545

    to export and import

---
+++ app.js
@@ -0,0 +1,13 @@
+//Two kind of exports:
+//named 
+//deafult
+
+const a =10
+
+export default a
+//here you can import in script js whatever you want because only a will be import from here
+
+//Two kind of exports:
+//named 
+//deafult
+
--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// var h1=document.createElement('h1');
+// //console
+//  h1.innerHTML= "Hello from JS";
+//  //console.log(h1)
 
-var h1=document.createElement('h1');
-//console
- h1.innerHTML= "Hello from JS";
- //console.log(h1)
+// //browser
+// document.body.appendChild(h1);
 
-//browser
-document.body.appendChild(h1);
+
+import whatever from "./app"
+console.log(a);
+
