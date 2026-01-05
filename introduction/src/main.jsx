602219f688cf52d929eff943cdff00c92ea0f7a2
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-05 21:36:24 +0545

    rendering

--- introduction/src/main.jsx
+++ introduction/src/main.jsx
@@ -3,8 +3,5 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')).render(<App/>)
+
