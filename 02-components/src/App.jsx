5cf4574d132de5d134821bfc402295fa3f1c0e82
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-07 22:48:53 +0545

    learning about components bases architecture

---
+++ 02-components/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      <div className='card'>
+        <h1>Swikar Gyawali</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iure, ratione.</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
