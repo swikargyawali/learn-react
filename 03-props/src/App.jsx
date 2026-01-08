035cd7bf652646a07c0d3a4b6f3b64966bcd8381
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-08 20:42:26 +0545

    understanding props

---
+++ 03-props/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+      <div className='card'>
+        <img src="https://plus.unsplash.com/premium_photo-1764695440099-2c5e152e950a?q=80&w=1025&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+        <h1>Swikar Gyawali</h1>
+        <p>Hey, I am Swikar Gyawali. <br />
+          A passionate backend engineer</p>
+        <button>View profile</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
