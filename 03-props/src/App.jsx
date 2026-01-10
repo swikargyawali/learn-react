178218cbd2296f40734a847f736d978040ddf4bf
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-10 13:38:03 +0545

    changing context

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -5,8 +5,9 @@ const App = () => {
   return (
     <div className='parent'>
 
-      <Card user='Bibek' age={24}/>
-      <Card user='Sarthak' age={21}/>
+      <Card user='swikar' age= {20}  img='https://plus.unsplash.com/premium_photo-1757914024397-8dfdb7756af1?q=80&w=943&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+
+       <Card user='Firoj' age={24} img='https://plus.unsplash.com/premium_photo-1741669281773-7ffdefea68be?q=80&w=967&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
       
     </div>
   )
--- 03-props/src/components/card.jsx
+++ 03-props/src/components/card.jsx
@@ -5,11 +5,12 @@ const card = (props) => {
   console.log(props)
   return (
     <div className='card'>
-        <img src="https://scontent.fbwa8-1.fna.fbcdn.net/v/t39.30808-6/571117920_1515124999637137_2966088716492644335_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=bvBhumF723cQ7kNvwGlUvF3&_nc_oc=Adm3BpKxbeOBtWoCCllN903NGD6a6M7ejWhfUejRTU9RDDRXitPn_TkWdfjU-T2814U&_nc_zt=23&_nc_ht=scontent.fbwa8-1.fna&_nc_gid=BTHTD8AAdeWWkhD9P5lWZA&oh=00_AfpdN8KWWk26qX8zU4ERc6yvDR_1LmmHcz-GaZI2kaplNg&oe=69658D57" alt="" />
-        <h1><props className="user"></props></h1>
-        <p>Hey, I am {props.user}. <br />
+        <img src={props.img} alt=''></img>
+        <h1>{props.user}</h1>
+        <p>Hey, I am {props.user}, {props.age}. <br />
           A passionate backend engineer</p>
         <button>View profile</button>
+
     </div>
   )
 }
