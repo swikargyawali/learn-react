278aba9adc5342fbf58e10ca3646f37447523c2b
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-18 21:51:38 +0545

    changes

--- 03-props/src/App.jsx
+++ 03-props/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className='parent'>
 
-      <Card user='swikar' age= {20}  img='https://plus.unsplash.com/premium_photo-1757914024397-8dfdb7756af1?q=80&w=943&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
+      <Card user='Swikar' age= {20}  img='https://plus.unsplash.com/premium_photo-1757914024397-8dfdb7756af1?q=80&w=943&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'/>
 
        <Card user='Firoj' age={24} img='https://plus.unsplash.com/premium_photo-1741669281773-7ffdefea68be?q=80&w=967&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' />
       
