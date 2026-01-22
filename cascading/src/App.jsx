35100f5d5182f146d2f79bc5ce49b23421b835ef
Swikar Gyawali <swikargyawali710@gmail.com>
2026-01-22 14:04:32 +0545

    CSS

---
+++ cascading/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Button from './components/button/Button'
+import Header from './components/header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <h1><Button/></h1>
+      <h1><Header/></h1>
+    </div>
+  )
+}
+
+export default App
---
+++ cascading/src/components/button/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+
+const Button = () => {
+  return (
+    <div className={styles.btn}>
+      <h1>This is Button</h1>
+
+    </div>
+  )
+}
+
+export default Button
